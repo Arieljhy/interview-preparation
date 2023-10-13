a313b505ced2864a21f8465ae8271d934fa83cdc
Arieljhy <ariel_jhy@163.com>
2023-10-13 20:48:42 +0800

    -

--- 基础/JS/LRU.js
+++ 基础/JS/LRU.js
@@ -24,24 +24,4 @@ class LRU {
     }
 }
 
-class LRU{
-    constructor(count){
-        this.maxCount = count;
-        this.cache = new Map();
-    }
-    put(key,value){
-        if(this.cache.size === this.maxCount) this.cache.delete(this.cache.keys().next().value);
-        this.cache.set(key,value);
-    }
-    get(key){
-        if(this.cache.has(key)){
-           let val =  this.cache.get(key)
-           this.cache.delete(key);
-           this.cache.set(key,val);
-           return val;
-        }else{
-            return false;
-        }
-        
-    }
-}
+
